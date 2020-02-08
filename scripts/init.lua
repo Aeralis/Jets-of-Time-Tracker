9b40e3aef5765f46252f516bd673b9cef0e33442
Aeralis <55809582+Aeralis@users.noreply.github.com>
2020-02-07 21:08:58 -0800

    Add files via upload
    Fixed syntax errors to allow loading.

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,9 +1,5 @@
 Tracker:AddItems("items/items.json")
-if (string.find(Tracker.ActiveVariantUID, "upcoming")) then
-	Tracker:AddMaps("maps/maps.json")
-	Tracker:AddLocations("upcoming/locations/locations.json")
-	Tracker:AddLayouts("layouts/tracker.json")
-else	
+
 if not (string.find(Tracker.ActiveVariantUID, "items_only")) then
 	Tracker:AddMaps("maps/maps.json")
 	Tracker:AddLocations("locations/locations.json")
@@ -11,5 +7,5 @@ if not (string.find(Tracker.ActiveVariantUID, "items_only")) then
 else
 	Tracker:AddLayouts("items_only/layouts/tracker.json")
 end
-end
+
 Tracker:AddLayouts("layouts/broadcast.json")
