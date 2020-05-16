7af490ab0334ff4cf44399d63a1cb62a39de71b9
TheBigSalarius <edwardmask@gmail.com>
2020-05-15 21:44:46 -0400

    init file fix

--- scripts/init.lua
+++ scripts/init.lua
@@ -9,3 +9,9 @@ else
 end
 
 Tracker:AddLayouts("layouts/broadcast.json")
+
+if _VERSION == "Lua 5.3" then
+    ScriptHost:LoadScript("scripts/autotracking.lua")
+else    
+    print("Auto-tracker is unsupported by your tracker version")
+end
