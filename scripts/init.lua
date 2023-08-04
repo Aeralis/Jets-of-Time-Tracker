d52cd5b6d9cd8ecfd03b58701f9ef84c7d95e9e3
C <3493534+coffeemancy@users.noreply.github.com>
2023-08-04 16:16:55 -0400

    Luacheck and whitespace cleanup (#1)
    * consistent EOL characters for windows/mac/linux devs
    
    Set .gitattributes to make sure line endings are converted on
    checkout based on platform (i.e. CR+LF for Windows, LF for
    Mac/Linux).
    
    * github workflow to run luacheck on changes to .lua files
    
    * do not add .git* files or *.zip during `git archive`
    
    * tabs->spaces and remove extraneous whitespace from lua files
    
    * run luacheck on workflow changes, ignore globals for now
    
    * set higher max line length, as code currently meets
    
    * pass luacheck
    
    ---------
    
    Co-authored-by: C <caltronnotlrac@gmail.com>

--- scripts/init.lua
+++ scripts/init.lua
@@ -6,28 +6,28 @@ if string.find(Tracker.ActiveVariantUID, "items_only") then
   Tracker:AddLayouts("items_only/layouts/tracker.json")
   Tracker:AddLayouts("layouts/broadcast.json")
 elseif string.find(Tracker.ActiveVariantUID, "chronosanity") then
-	Tracker:AddLocations("chronosanity/locations/locations.json")
-	Tracker:AddLayouts("chronosanity/layouts/tracker.json")
+  Tracker:AddLocations("chronosanity/locations/locations.json")
+  Tracker:AddLayouts("chronosanity/layouts/tracker.json")
   Tracker:AddLayouts("chronosanity/layouts/broadcast.json")
 elseif string.find(Tracker.ActiveVariantUID, "lost_world_items") then
   Tracker:AddLayouts("lost_world_items/layouts/tracker.json")
-	Tracker:AddLayouts("lost_world_items/layouts/broadcast.json")
+  Tracker:AddLayouts("lost_world_items/layouts/broadcast.json")
 elseif string.find(Tracker.ActiveVariantUID, "lost_worlds") then
   Tracker:AddLocations("lost_worlds/locations/locations.json")
-	Tracker:AddLayouts("lost_worlds/layouts/tracker.json")
-	Tracker:AddLayouts("lost_worlds/layouts/broadcast.json")
+  Tracker:AddLayouts("lost_worlds/layouts/tracker.json")
+  Tracker:AddLayouts("lost_worlds/layouts/broadcast.json")
 elseif string.find(Tracker.ActiveVariantUID, "vanilla_rando") then
   Tracker:AddLocations("vanilla_rando/locations/locations.json")
-	Tracker:AddLayouts("vanilla_rando/layouts/tracker.json")
-	Tracker:AddLayouts("vanilla_rando/layouts/broadcast.json")
+  Tracker:AddLayouts("vanilla_rando/layouts/tracker.json")
+  Tracker:AddLayouts("vanilla_rando/layouts/broadcast.json")
 else
-	Tracker:AddLocations("locations/locations.json")
-	Tracker:AddLayouts("layouts/tracker.json")
+  Tracker:AddLocations("locations/locations.json")
+  Tracker:AddLayouts("layouts/tracker.json")
   Tracker:AddLayouts("layouts/broadcast.json")
 end
 
 if _VERSION == "Lua 5.3" then
     ScriptHost:LoadScript("scripts/autotracking.lua")
-else    
+else
     print("Auto-tracker is unsupported by your tracker version")
 end
