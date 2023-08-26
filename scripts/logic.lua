8a34cd133a5ac0278124da4159c7bf3ac2113dc0
C <caltronnotlrac@gmail.com>
2023-08-26 12:02:38 -0400

    consolidate std/LoC locations, mode-based access/visibility rules

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -8,6 +8,10 @@ function legacyOfCyrusMode()
   return string.find(Tracker.ActiveVariantUID, "legacy_of_cyrus") ~= nil
 end
 
+function notLegacyOfCyrusMode()
+  return not legacyOfCyrusMode()
+end
+
 -- Check if the tracker is in Lost Worlds mode
 function lostWorldsMode()
   return string.find(Tracker.ActiveVariantUID, "lost_world") ~= nil
@@ -28,6 +32,67 @@ function canAccessSealed()
   local lwMode = lostWorldsMode()
 
   return ((dragonTank or (locMode and pendant)) and earlyPendant) or (pendant and (magus or blackTyrano or lwMode))
+end
+
+function canAccessMagusCastle()
+  local frog = Tracker:FindObjectForCode("frog").Active
+  local masamune = Tracker:FindObjectForCode("masamune").Active
+
+  if legacyOfCyrusMode() then
+    local magus = Tracker:FindObjectForCode("magus").Active
+    return frog and magus and masamune
+  end
+
+  return frog and masamune
+end
+
+function canAccessNorthernRuins()
+  if not canFly() then
+    return false
+  end
+
+  local grandleon = Tracker:FindObjectForCode("grandleon").Active
+
+  if legacyOfCyrusMode() then
+    local frog = Tracker:FindObjectForCode("frog").Active
+    local magus = Tracker:FindObjectForCode("magus").Active
+    return frog and magus and grandleon
+  end
+
+  return grandleon
+end
+
+function canAccessOzzieFort()
+  if not canFly() then
+    return false
+  end
+
+  if legacyOfCyrusMode() then
+    local frog = Tracker:FindObjectForCode("frog").Active
+    local magus = Tracker:FindObjectForCode("magus").Active
+    local magusboss = Tracker:FindObjectForCode("magusboss").Active
+    return frog and magus and magusboss
+  end
+
+  local pendant = Tracker:FindObjectForCode("pendant").Active
+  local gatekey = Tracker:FindObjectForCode("gatekey").Active
+
+  return pendant or gatekey
+end
+
+
+function couldAccessOceanPalace()
+  return not legacyOfCyrusMode()
+end
+
+
+
+function couldAccessTyranoCastle()
+  return not legacyOfCyrusMode()
+end
+
+function couldAccessSunKeep()
+  return not legacyOfCyrusMode()
 end
 
 function canFly()
